f557c3d677802baaaddfe6cc1840f3aeab0171a9
jony <habibun.noby@gmail.com>
2006-09-29 06:19:44 +0600

    summary

---
+++ beginning-php/09-handling_html_forms_with_php/summary.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 10/23/15
+ * Time: 5:14 PM
+ */
+?>
+❑
+How to create Web forms, including all the different types of controls that you can place in
+a form
+❑
+All about the $_GET, $_POST, and $_REQUEST superglobals, and how your scripts can use them
+to capture form data sent by a user
+❑
+Some of the security issues surrounding Web forms, and how to mitigate them
+❑
+How to deal with empty form fields
+❑
+How to get the PHP engine to recognize multi-value fields, and how to read the data that these
+fields send
+❑
+How to generate Web forms from within your PHP scripts. This allows you to add more
+interactivity and flexibility to your forms. You worked through an example of creating such an
+interactive form
+❑
+The concept of hidden form fields and how to use them to store data between page requests.
+You used this technique to create a three-stage registration form
+❑
+How to handle files uploaded via Web forms, including using the $_FILES superglobal to read
+information about uploaded files
+❑
+How to redirect the browser after a form submission in order to display a thank-you page and
+avoid issues with page reloads
+
+
+
