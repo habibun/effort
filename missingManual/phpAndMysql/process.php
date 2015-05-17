00c55898815ad194e1a6373d31bd5644bfb0b07b
Habibun Noby <habibun.noby@gmail.com>
2015-05-18 00:00:15 +0600

    Writing a Simple PHP Connection Script

--- missingManual/phpAndMysql/process.php
+++ missingManual/phpAndMysql/process.php
@@ -3,9 +3,25 @@
 </head>
 <body>
     <?php
-    foreach ($_REQUEST as $key => $value) {
-        echo '<p>'.$key .':'. $value . '</p>';
+        mysql_connect("localhost", "root", "") or die("<p>Error connecting to database: " . mysql_error() . "</p>");
+        echo "<p>Connected to MySQL!</p>";
+
+        mysql_select_db('effort') or die('<p>Error selecting the database effort: '.mysql_error().'</p>');
+        echo '<p>Connected to MySQL, using database effort.</p>';
+
+        $result = mysql_query('show tables;');
+
+    if (!$result) {
+        die("<p>Error in listing tables: " . mysql_error() . "</p>");
     }
+
+//    print_r ($result);
+    echo '<p>Tables in database: '.'</p>';
+    echo '<ul>';
+    while ($row = mysql_fetch_array($result)) {
+        echo "<li>Table: {$row[0]}</li>";
+    }
+    echo '</ul>';
     ?>
 </body>
 </html>
