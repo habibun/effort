ee30578d0c04213a6215c9cbff171963cb4065ef
Habibun Noby <habibun.noby@gmail.com>
2015-05-18 01:13:41 +0600

    cache clear all

--- MissingManual/javaScript/livingExamples/chapter08/superfish/complete_siteJS.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.nav').superfish({
-    delay: 400
-	});
-  $('.nav li:has(ul)').find('a:first').addClass('sub');
-  $('.nav ul li:has(ul)').find('a:first').removeClass('sub').addClass('subsub');
-});
