3d3c8b9e6b5a8cce2f9067917e79687101926e76
Habibun Noby <habibun.noby@gmail.com>
2015-05-22 21:52:10 +0600

    when things go wrong and they will

---
+++ missingManual/phpAndMysql/C08-WhenThingsGoWrongAndTheyWill/PlanningYourErrorPages.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Habibun
+ * Date: 22/5/2015
+ * Time: 9:47 PM
+ */
--- missingManual/phpAndMysql/process.php
+++ missingManual/phpAndMysql/process.php
@@ -1,37 +1,60 @@
-<html>
-<head>
-</head>
-<body>
 <?php
+
 require 'database_connection.php';
-$user_id = 6;
+
+// Get the user ID of the user to show
+$user_id = $_REQUEST['user_id'];
+
+// Build the SELECT statement
 $select_query = "SELECT * FROM php_and_mysql_users WHERE user_id = " . $user_id;
 
+// Run the query
 $result = mysql_query($select_query);
-
-
+if ($result) {
     $row = mysql_fetch_array($result);
-    $first_name = $row['first_name'];
-    $last_name = $row['last_name'];
-    $email = $row['email'];
-    $facebook_url = $row['facebook_url'];
-    $twitter_url = $row['twitter_url'];
+    $first_name     = $row['first_name'];
+    $last_name      = $row['last_name'];
+    $bio            = preg_replace("/[\r\n]+/", "</p><p>", $row['bio']);
+//    $bio            = preg_replace("/[\r\n]+/", "</p><p>", $row['bio']);
+    $email          = $row['email'];
+    $facebook_url   = $row['facebook_url'];
+    $twitter_url = $row['twitter_handle'];
 
 
-
-
+    // To be added later
+    $user_image = "../../images/missing_user.png";
+} else {
+    die("Error locating user with ID {$user_id}");
+}
 
 ?>
+
+<html>
+<head>
+    <link href="../../css/phpMM.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+<div id="example">User Profile</div>
+
 <div id="content">
-    <p>Here's a record of what information you submitted:</p>
-    <p>
-        Name: <?php echo $first_name . " " . $last_name; ?><br />
-        E-Mail Address: <?php echo $email; ?><br />
-        <a href="<?php echo $facebook_url; ?>">Your Facebook page</a>
-        <br />
-        <a href="<?php echo $twitter_url; ?>">Check out your Twitter feed</a>
-        <br />
-    </p>
+    <div class="user_profile">
+        <h1><?php echo "{$first_name} {$last_name}"; ?></h1>
+        <p><img src="<?php echo $user_image; ?>" class="user_pic" />
+            <?php echo $bio; ?></p>
+        <p class="contact_info">Get in touch with <?php echo $first_name; ?>:</p>
+        <ul>
+            <li>...by emailing them at
+                <a href="<?php echo $email; ?>"><?php echo $email; ?></a></li>
+            <li>...by
+                <a href="<?php echo $facebook_url; ?>">checking them out
+                    on Facebook</a></li>
+            <li>...by <a href="<?php echo $twitter_url; ?>">following them
+                    on Twitter</a></li>
+        </ul>
+    </div>
 </div>
+<div id="footer"></div>
 </body>
 </html>
