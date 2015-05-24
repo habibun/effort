cfd2f68cab7597e5811abc7ecee12de05dadefb1
Habibun Noby <habibun.noby@gmail.com>
2015-05-24 20:50:40 +0600

    storing different objects in different table

---
+++ missingManual/phpAndMysql/C10-BinaryObjectsAndImageLoading/StoringDiffrentObjectsInDiffrentTables.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Habibun
+ * Date: 24/5/2015
+ * Time: 8:20 PM
+ */
+//a mysql query
+
+/*
+ CREATE TABLE images (
+ image_id int AUTO_INCREMENT PRIMARY KEY,
+ filename varchar(200) NOT NULL,
+ mime_type varchar(50) NOT NULL,
+ file_size int NOT NULL,
+ image_data mediumblob NOT NULL
+);
+*/
+
+//database column type blob stand for binary large object
