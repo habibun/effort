a315ac5c2b93de8256ffec8b3f17702617c75ef4
Habibun Noby <habibun.noby@gmail.com>
2015-05-26 21:33:03 +0600

    so which approach is best

---
+++ missingManual/phpAndMysql/C10-BinaryObjectsAndImageLoading/SoWhichApproachIsBest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Habibun
+ * Date: 26/5/2015
+ * Time: 9:28 PM
+ */
+
+/**
+ * drop column from table
+ *
+ * ALTER TABLE php_and_mysql_users DROP COLUMN profile_pic_id;
+ */
+
+
+/**
+ * drop table from database
+ *
+ * DROP TABLE php_and_mysql_users;
+ */
