8a17af38d1952fb875360d78bbc9fbbbbfcd76bf
Habibun Noby <habibun.noby@gmail.com>
2015-05-30 15:13:45 +0600

    password dont belong php script

---
+++ missingManual/phpAndMysql/C12-AuthenticationAndAuthorization/PasswordsDoNotBelongInPhpScripts.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Habibun
+ * Date: 30/5/2015
+ * Time: 11:50 AM
+ */
+
+/**
+ * add two column
+ *
+ * alter table php_and_mysql_users add username VARCHAR(32) NOT NULL after user_id,
+add password VARCHAR(16) NOT NULL after username;
+ */
+
+
+/**
+ * check return row
+ *
+ * if (mysql_num_rows($results) == 1)
+ */
