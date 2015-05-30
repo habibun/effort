3c3e3edbd768d94c7986dde0c08216efe1ec9d6a
Habibun Noby <habibun.noby@gmail.com>
2015-05-30 17:15:54 +0600

    authentication and authorization

--- missingManual/phpAndMysql/C12-AuthenticationAndAuthorization/PasswordsCreateSecurityButShouldBeSecure.php
+++ missingManual/phpAndMysql/C12-AuthenticationAndAuthorization/PasswordsCreateSecurityButShouldBeSecure.php
@@ -5,3 +5,22 @@
  * Date: 30/5/2015
  * Time: 3:14 PM
  */
+
+/**
+ *
+ * Passwords Create Security, But Should Be Secure
+ *
+ * crypt($password);
+ */
+
+/**
+ * test salt
+ */
+
+$input = "secret_string";
+$salt = "salt";
+$first_output = crypt($input, $salt);
+$second_output = crypt($input, $salt);
+
+echo "First output is {$first_output}\n\n";
+echo "Second output is {$second_output}\n\n";
