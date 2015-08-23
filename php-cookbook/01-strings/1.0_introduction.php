3359d912261252231834a5b3ad469ef675b177f7
Habibun Noby <habibun.noby@gmail.com>
2015-08-24 00:28:38 +0600

    introduction

--- php-cookbook/01-strings/1.0_introduction.php
+++ php-cookbook/01-strings/1.0_introduction.php
@@ -15,3 +15,47 @@ print
     'Would you pay $1.75 for 8 ounces of tap water?';
 print
     'In double-quoted strings, newline is represented by \n';
+
+print '<br>';
+
+//text using double quote
+print
+    "I've gone to the store.";
+print
+    "The sauce cost \$10.25.";
+$cost
+    = '$10.25';
+print
+    "The sauce cost $cost.";
+print
+    "The sauce cost \$\061\060.\x32\x35.";
+
+print '<br>';
+
+//text using heredoc
+print <<< END
+It's funny when signs say things like:
+Original "Root" Beer
+"Free" Gift
+Shoes cleaned while "you" wait
+or have other misquoted words.
+END;
+
+//For example, if you’re using jQuery:
+$js = <<<'__JS__'
+$.ajax({
+'url': '/api/getStock',
+'data': {
+'ticker': 'LNKD'
+},
+'success': function( data ) {
+$( "#stock-price" ).html( "<strong>$" + data + "</strong>" );
+}
+});
+__JS__;
+print $js;
+
+// Getting an individual byte in a string
+$neighbor = 'Hilda';
+print $neighbor[3];
+
