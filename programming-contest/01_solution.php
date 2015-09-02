bef13d08ad601243e54db96b0514e2315e804e9f
Habibun Noby <habibun.noby@gmail.com>
2015-09-02 21:07:20 +0600

    comparing floating point numbers

--- programming-contest/01_solution.php
+++ programming-contest/01_solution.php
@@ -8,7 +8,21 @@
 
 function draw($n)
 {
+    if ($n == 1) {
+        $zero = 0;
+        $one = strlen($n);
+//    echo $nLenght;
+        for ($i= 0; $i < $n; $n--) {
+            echo '<pre>' . $zero . $zero. $n. $zero. $zero.  '</pre>';
+        }
+    }
+    if ($n == 3) {
 
+    }
+
+    if ($n == 5) {
+
+    }
 }
 
-draw(3);
+draw(1);
