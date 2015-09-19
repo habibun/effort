4ec9f8982f700d3e4dd7c7e48c18ae7173e7a72f
Habibun Noby <habibun.noby@gmail.com>
2015-09-19 11:04:09 +0600

    creating arrays

---
+++ beginning-php/06-arrays/creating_arrays.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 9/19/15
+ * Time: 10:23 AM
+ */
+
+/**
+ * Indexed array
+ */
+$authors = array('Steinbeck', 'Kafka', 'Tolkien', 'Dickens');
+
+/**
+ * Associative array
+ */
+$myBook = array(
+    "title" => "The Grapes of Wrath",
+    "author" => "John Steinbeck",
+    "pubYear" => 1939
+);
