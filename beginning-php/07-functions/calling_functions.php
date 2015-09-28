1ec50531c7b7f4549d237aa7d1ab9311b490b362
jony <habibun.noby@gmail.com>
2015-09-28 19:57:11 +0600

    calling functions

---
+++ beginning-php/07-functions/calling_functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 9/27/15
+ * Time: 8:53 PM
+ */
+
+//To call a function, you write the function name, followed by an opening and a closing parenthesis:
+functionName();
+
+
+/*
+ * If you need to pass arguments to the function, place them between the parentheses, separating each
+argument by commas:
+ */
+functionName( argument );
+functionName( argument1, argument2 );
+
+//If a function returns a value, you can assign the value to a variable:
+$returnValue = functionName();
+
+//You can also pass the return value directly to another function, such as print():
+print( functionName( argument ) );
+
+
+//how to work function
+echo "The square root of 9 is: " . sqrt( 9 ) . ".<br/>";
+echo "All done!<br/>";
