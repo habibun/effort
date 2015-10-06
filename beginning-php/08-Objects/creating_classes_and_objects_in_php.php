fe12f8c00a3edc2a27af931bbde8cbd209d39703
jony <habibun.noby@gmail.com>
2015-10-06 20:34:59 +0600

    creating classes and objects in php

---
+++ beginning-php/08-Objects/creating_classes_and_objects_in_php.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 10/5/15
+ * Time: 10:51 PM
+ */
+
+//Here's an example that shows how to create objects
+
+Class Car {
+    //nothing to see here; move along
+}
+
+$beetle = new Car();
+$mustang = new Car();
+
+print_r($beetle);
+print_r($mustang);
