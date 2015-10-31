50cfee4106853f356ce19da7a1ed07e8ede36835
jony <habibun.noby@gmail.com>
2015-10-31 21:21:25 +0600

    continue session 3

--- beginning-php/10-preserving-state-with-query-strings-cookies-and-sessions/using_php_sessions_to_store_data.php
+++ beginning-php/10-preserving-state-with-query-strings-cookies-and-sessions/using_php_sessions_to_store_data.php
@@ -1,117 +1,120 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jony
- * Date: 10/28/15
- * Time: 8:18 PM
- */
-
-/**
- * Creating a Session
- */
+error_reporting(-1);
+ini_set('display_errors', 'On');
 session_start();
 
-/**
- * Reading and Writing Session Data
- */
-//so you might store the user's first name using
-$_SESSION['firstName'] = "John";
-
-//echo $_SESSION['firstName'];
-
-//you can store any king of data in sessions, including arrays and objects:
-$userDetails = array("firstName" => "John","lastName" => "Smith", "age" => 34);
-
-$_SESSION['userDetails'] = $userDetails;
-?>
-
-<?php
-/**
- * However, if storing objects make sure you include your class definitions (or class definition files) before
-trying to retrieve the objects from the $_SESSION array, so that the PHP engine can correctly identify the
-objects when they’re retrieved:
- */
-session_start();
-
-class WebUser2 {
-    public $firstName;
-    public $lastName;
-}
-
-if(isset($_SESSION['user'])){
-    //Make sure the WebUser class is defined by this point
-    print_r($_SESSION['user']);
-}else{
-    echo "Creating user...";
-    $user = new WebUser2();
-    $user->firstName = "John";
-    $user->lastName = "Smith";
-    $_SESSION['user'] = $user;
-}
-?>
-<?php
-/**
- *  Create a Simple Shopping Cart
- */
-
-session_start();
-
-class Product2{
+class Product {
     private $productId;
     private $productName;
     private $price;
 
-    public function __construct($productId, $productName, $price)
-    {
+    public function __construct( $productId, $productName, $price ) {
         $this->productId = $productId;
         $this->productName = $productName;
         $this->price = $price;
     }
 
-    public function getId()
-    {
+    public function getId() {
         return $this->productId;
     }
 
-    public function getProductName()
-    {
+    public function getName() {
         return $this->productName;
     }
 
-    public function getPrice()
-    {
+    public function getPrice() {
         return $this->price;
     }
+
 }
 
 $products = array(
-    1 => new Product2(1,"SuperWidget",19.99),
-    2 => new Product2(2,"MegaWidget", 29.99),
-    3 => new Product2(3,"WonderWidget", 39.99)
+    1 => new Product( 1, "SuperWidget", 19.99 ),
+    2 => new Product( 2, "MegaWidget", 29.99 ),
+    3 => new Product( 3, "WonderWidget", 39.99 )
 );
 
-if(!isset($_SESSION["cart"])) $_SESSION["cart"] = array();
+if ( !isset( $_SESSION["cart"] ) ) $_SESSION["cart"] = array();
 
-if(isset($_GET["action"]) and $_GET["action"] == "addItem"){
+if ( isset( $_GET["action"] ) and $_GET["action"] == "addItem" ) {
     addItem();
-}elseif(isset($_GET["action"]) and $_GET["action"] == "removeItem"){
+} elseif ( isset( $_GET["action"] ) and $_GET["action"] == "removeItem" ) {
     removeItem();
-}else{
+} else {
     displayCart();
 }
 
-function addItem()
-{
+function addItem() {
     global $products;
-    if (isset($_GET["productId"]) and $_GET["productId"] >= 1 and $_GET["productId"] <= 3){
+    if ( isset( $_GET["productId"] ) and $_GET["productId"] >= 1 and $_GET["productId"] <= 3 ) {
         $productId = (int) $_GET["productId"];
 
-        if(!isset($_SESSION["cart"][$productId])){
+        if ( !isset( $_SESSION["cart"][$productId] ) ) {
             $_SESSION["cart"][$productId] = $products[$productId];
         }
     }
+
     session_write_close();
-    header("Location: using_php_sessions_to_store_data.php");
+    header( "Location: shopping_cart.php" );
 }
 
+function removeItem() {
+    global $products;
+    if ( isset( $_GET["productId"] ) and $_GET["productId"] >= 1 and $_GET["productId"] <= 3 ) {
+        $productId = (int) $_GET["productId"];
 
+        if ( isset( $_SESSION["cart"][$productId] ) ) {
+            unset( $_SESSION["cart"][$productId] );
+        }
+    }
+
+    session_write_close();
+    header( "Location: shopping_cart.php" );
+}
+
+function displayCart() {
+global $products;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>A shopping cart using sessions</title>
+    <link rel="stylesheet" type="text/css" href="../living-examples/ch09/common.css" />
+</head>
+<body>
+
+<h1>Your shopping cart</h1>
+
+<dl>
+
+    <?php
+    $totalPrice = 0;
+    foreach ( $_SESSION["cart"] as $product ) {
+        $totalPrice += $product->getPrice();
+        ?>
+        <dt><?php echo $product->getName() ?></dt>
+        <dd>$<?php echo number_format( $product->getPrice(), 2 ) ?>
+            <a href="using_php_sessions_to_store_data.php?action=removeItem&amp;productId=<?php echo $product->getId() ?>">Remove</a></dd>
+    <?php } ?>
+    <dt>Cart Total:</dt>
+    <dd><strong>$<?php echo number_format( $totalPrice, 2 ) ?></strong></dd>
+</dl>
+
+<h1>Product list</h1>
+
+<dl>
+    <?php foreach ( $products as $product ) { ?>
+        <dt><?php echo $product->getName() ?></dt>
+        <dd>$<?php echo number_format( $product->getPrice(), 2 ) ?>
+            <a href="using_php_sessions_to_store_data.php?action=addItem&amp;productId=<?php echo $product->getId() ?>">Add Item</a></dd>
+    <?php } ?>
+</dl>
+
+<?php
+}
+
+?>
+
+</body>
+</html>
