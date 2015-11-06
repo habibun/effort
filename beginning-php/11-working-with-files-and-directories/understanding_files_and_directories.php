ba7682132f1dc10ae68aab85971f2f122b7e7345
jony <habibun.noby@gmail.com>
2015-11-06 12:50:13 +0600

    understanding the files and directories

---
+++ beginning-php/11-working-with-files-and-directories/understanding_files_and_directories.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 11/6/15
+ * Time: 12:39 PM
+ */
+
+/*
+UNIX-based systems such as Linux use slashes to delimit elements in a path, like this:
+/home/matt/data/data.txt
+
+Windows uses backslashes:
+C:\MyDocs\data\data.txt
+ */
