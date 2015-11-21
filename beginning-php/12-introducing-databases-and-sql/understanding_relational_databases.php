f10ea0400bd216616b47b0dcd0777fd12bf222e9
Habibun Noby <habibun.noby@gmail.com>
2015-11-21 20:40:08 +0600

    understanding relation databases

---
+++ beginning-php/12-introducing-databases-and-sql/understanding_relational_databases.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 11/21/15
+ * Time: 3:26 PM
+ */
+
+/**
+ * Select
+ *
+ * Retrieves data from one or more table
+ */
+
+/**
+ * INSERT
+ *
+ * inserts data into table
+ */
+
+/**
+ * REPLACE
+ *
+ * replace data into table
+ */
+
+/**
+ * UPDATE
+ *  updates data in a table
+ */
+
+/**
+ * DELETE
+ *
+ *  delete data from a table
+ */
+
+/**
+ * CREATE
+ *
+ * create a database, table or index
+ */
+
+/**
+ * ALTER
+ *
+ * modify the structure of a table
+ */
+
+/**
+ * DROP
+ *
+ * wipes out a database or table
+ */
+
