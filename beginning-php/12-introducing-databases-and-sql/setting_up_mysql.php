abc645f8799970e3e701ef83b1d7a5f6ac8040c8
Habibun Noby <habibun.noby@gmail.com>
2015-11-21 21:41:31 +0600

    setting up mysql

---
+++ beginning-php/12-introducing-databases-and-sql/setting_up_mysql.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 11/21/15
+ * Time: 8:49 PM
+ */
+
+/**
+ *  Inspect the current privileges — Enter the following at the mysql> prompt and press Enter:
+ */
+//SELECT Host, User, Password FROM mysql.user;
