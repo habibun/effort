87f22936d3d375acc4c3a4d251d5ff166531d97f
Habibun Noby <habibun.noby@gmail.com>
2015-11-25 23:09:09 +0600

    cp 12 summary

---
+++ beginning-php/12-introducing-databases-and-sql/summary.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 11/25/15
+ * Time: 11:00 PM
+ */
+?>
+
+❑
+You studied embedded, client-server, simple, and relational databases, and saw that a relational
+database such as MySQL stores its data as rows and columns in tables. You discovered that by
+splitting your data over more than one table, you can make your database more efficient — a
+process known as normalization
+
+❑
+In preparation for working with MySQL, you learned the basics of the SQL language, and
+explored MySQL data types and indexes (keys). You also looked at the concept of NULL values
+
+❑
+Putting theory into practice, you learned how to start your MySQL server, set up a root
+password, create databases and tables, add data to tables, and read, update, and delete data in
+tables. You also learned how to delete whole tables and databases
+
+❑
+In the final section of the chapter, you took a quick look at connecting to MySQL from your PHP
+scripts using the PDO extension. You learned how to set up a connection, how to handle errors,
+and how to read data from a table
+
