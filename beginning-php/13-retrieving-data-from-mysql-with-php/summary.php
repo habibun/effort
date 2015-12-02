f2c0f310a1666bdebcb3e7fbd2176f7cd2df7391
Habibun Noby <habibun.noby@gmail.com>
2015-12-02 20:05:37 +0600

    summary

---
+++ beginning-php/13-retrieving-data-from-mysql-with-php/summary.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 12/2/15
+ * Time: 12:44 AM
+ */
+?>
+
+❑
+First you set up the tables for an imaginary book club database that you used throughout the
+chapter. Along the way, you explored the BINARY attribute and case-sensitivity; the UNIQUE
+constraint for enforcing unique values in a column; the ENUM data type for creating fields with a
+small number of possible values; and the TIMESTAMP data type for automatically recording
+when records are created or updated
+
+❑
+Next, you took a closer look at the SQL SELECT statement. You learned how to use LIMIT to
+restrict the number of rows returned from a query, and how to sort the rows of a result set using
+the ORDER BY clause. You saw how to make queries more flexible by using the LIKE and NOT
+LIKE operators, and how to use functions such as count(), sum(), min(), max(), and avg() to
+summarize columns in a table
+
+
+❑
+Duplicate rows can be a problem in result sets, and you saw how to solve this issue by using the
+DISTINCT keyword. You also learned how to group results by a specified column or columns
+through the use of GROUP BY clauses
+
+
+❑
+One of the main advantages of a relational database is that you can pull data from more than
+one table at a time — a process known as joining tables. You learned how to do this, and also
+how to use aliases to make both queries and result sets more readable
+
+
+❑
+To round off the discussion on SELECT queries, you explored a few of the myriad MySQL
+operators and functions that you can use to add even more power to your queries
+
+
