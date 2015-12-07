fef5ed3dcc995cfdb979f9e0b5803a2861c59dfd
Habibun Noby <habibun.noby@gmail.com>
2015-12-07 19:54:37 +0600

    installing pear packages

---
+++ beginning-php/15-making-your-job-easier-with-pear/installing_pear_packages.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 12/6/15
+ * Time: 9:30 PM
+ */
+
+/*
+    pear install Net_UserAgent_Detect
+ */
+
+/**
+ * Installing Dependencies
+ */
+/*
+    --alldeps also installs optional packages that are related to the package you’re installing, but that
+    are not required for the package to work
+
+    pear install --alldeps Net_UserAgent_Detect
+ */
+
+/*
+    If you think this is more than you need, you can use
+    --onlyreqdeps to install just the required dependencies
+ */
+
+
+/**
+ * Uninstalling Packages
+ */
+
+/*
+     pear uninstall Net_UserAgent_Detect
+ */
