08a7b33b99293bd563a093de084f3944b8b9ae7d
Habibun Noby <habibun.noby@gmail.com>
2015-12-08 17:47:18 +0600

    echo and print

--- trial.php
+++ trial.php
@@ -7,6 +7,19 @@
  */
 
 //$basename =  basename( $_SERVER["PHP_SELF"] );
+//echo "starting-";
 $server = $_SERVER["PHP_SELF"];
 //echo $basename;
-echo $server;
+//echo $server;
+
+//$echo = echo "test";
+$print = print "test";
+
+print $print;
+
+$b = true;
+//$b ? print "true" : print "false";
+
+//$b ? echo "true" : echo "false";
+
+echo "hardy","test";
