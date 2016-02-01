d423243a492ba83766001726fa5a736e59f734d9
Habibun Noby <habibun.noby@gmail.com>
2016-02-01 16:00:37 +0600

    start regular expression

---
+++ beginning-php/18-string-matching-with-regular-expressions/what_is_a_regular_expression.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 2/6/16
+ * Time: 3:52 PM
+ */
+
+// For example, this
+//simple regular expression searches for the word “world” anywhere within the target string:
+
+/world/
+
