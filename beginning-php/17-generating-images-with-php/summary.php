c319b1d20c30ffe49f9a1f11c84dbc902d8c6f2a
Habibun Noby <habibun.noby@gmail.com>
2016-02-02 21:04:33 +0600

    finish summary

--- beginning-php/17-generating-images-with-php/summary.php
+++ beginning-php/17-generating-images-with-php/summary.php
@@ -5,3 +5,37 @@
  * Date: 1/30/16
  * Time: 8:53 PM
  */
+?>
+
+
+❑
+Learned the basics of computer images, including colors, coordinate systems, and image types
+
+
+❑
+Saw how to create bitmap images with imagecreate() and related functions, as well as
+allocate colors in an image
+
+
+❑
+Learned how to output images to the Web browser or to a file by using imagejpeg(),
+imagegif(), and imagepng()
+
+
+
+❑
+Looked at some of PHP’s drawing functions, including imagesetpixel(), imageline(),
+imagerectangle(), imageellipse(), imagearc(), and imagepolygon()
+
+
+
+❑
+Saw how to open existing images with imagecreatefromjpeg(), imagecreatefromgif(),
+and imagecreatefrompng() so that you can manipulate them. By way of example, you
+applied a watermark image to an existing photograph, and created a thumbnail image based on
+an existing image. Along the way you looked at the concepts of transparency and opacity
+
+
+
+❑
+Added text to an image using imagestring() and imagefttext()
