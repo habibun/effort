24762ce93d5a11d9640a53409ebfc415229ec354
Habibun Noby <habibun.noby@gmail.com>
2016-03-03 20:59:32 +0600

    summary

---
+++ beginning-php/20-writing-high-quality-code/summary.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 3/26/16
+ * Time: 8:42 PM
+ */
+?>
+
+
+❑
+How to write modular code: This involves splitting your code into small, easy-to-maintain
+chunks that can often be reused by different applications. You briefly revisited functions and
+classes, looked at PHP’s include(), require(), include_once(), and require_once()
+functions that let you split your application across different script files, and took a quick look at
+using namespaces to avoid clashing function, method, and constant names across code modules
+
+
+❑
+How to create and use coding standards: Coding standards help you write consistently
+formatted, readable code that’s easier to maintain. You looked at some of the conventions used
+in PHP and other languages
+
+
+❑
+Creating code documentation: You learned why comments and code documentation are an
+integral part of well-written applications, and studied how to write good comments and use
+phpDocumentor to generate documentation
+
+
+❑
+Application security: This important, often-overlooked aspect of Web programming is a critical
+part of any robust application. You looked at how to check and filter user input to ensure its
+integrity, as well as how to encode or escape your application’s output to ensure that it contains
+only safe data
+
+
+❑
+Error handling: For your application to behave as reliably as possible, it needs to handle
+problems gracefully. You saw how to use PHP’s error handling and logging functions to deal
+with errors, and how to use the power of exception classes to create flexible, robust error
+handling code
+
+
+❑
+Separating application and presentation code: You looked at how to move your presentation
+markup into separate template files, thereby creating a clean division between your
+application’s business logic and its visual interface. Doing this makes your application code
+much easier to work with, both for designers and for programmers
+
+
+❑
+Unit testing: You learned about the benefits of code testing in general, and unit testing in
+particular. Testing code early and often saves a lot of headaches further down the line. You
+looked briefly at PHPUnit, a testing framework that lets you easily write your own
+automated unit tests
+
+
+
+
+
+
+
+
+
+
+
+
