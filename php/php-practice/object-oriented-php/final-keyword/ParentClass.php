c4abd943e98694be79acdcdae461f66204ea6f27
Habibun Noby <habibun.noby@gmail.com>
2016-06-21 23:58:54 +0600

    final keyword

---
+++ php/php-practice/object-oriented-php/final-keyword/ParentClass.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 6/21/16
+ * Time: 11:43 PM
+ */
+//class ParentClass
+    /**
+     * Stop inheriting
+     */
+    final class ParentClass
+{
+//   public function parent(){
+
+    /**
+     * Prevents child classes from overriding by prefixing the definition with final.
+     */
+    public function parent(){
+        echo "I am a parent class";
+    }
+
+}
