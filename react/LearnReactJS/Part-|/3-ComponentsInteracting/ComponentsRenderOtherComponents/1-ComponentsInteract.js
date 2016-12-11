5656f90f73d7a07fb730454047c7fc290151a91e
Habibun Noby <habibun.noby@gmail.com>
2016-12-11 22:52:36 +0600

    components render other components

---
+++ react/LearnReactJS/Part-|/3-ComponentsInteracting/ComponentsRenderOtherComponents/1-ComponentsInteract.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jony on 12/10/16.
+ */
---
+++ react/LearnReactJS/Part-|/3-ComponentsInteracting/ComponentsRenderOtherComponents/2-AComponentInARenderFunction.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jony on 12/11/16.
+ */
+
+var OMG = React.createClass({
+    render: function () {
+        return <h1>Whooaa!</h1>;
+    }
+});
+
+var Crazy = React.createClass({
+    render: function () {
+        return <OMG />;
+    }
+});
