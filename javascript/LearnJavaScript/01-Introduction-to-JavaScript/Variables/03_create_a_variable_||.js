efd6007cfa7125348361ce60e17c5471bdecd88b
Habibun Noby <habibun.noby@gmail.com>
2016-12-17 21:40:39 +0600

    variables

---
+++ javascript/LearnJavaScript/01-Introduction-to-JavaScript/Variables/03_create_a_variable_||.js
@@ -0,0 +1,8 @@
+/**
+ * Created by habibun on 12/17/16.
+ */
+var strength = "50,000 pounds";
+var age = 25;
+var hasPet = true;
+console.log(age);
+console.log('How much stuff can a variable hold? ', strength);
