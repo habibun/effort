e30829bca6c8fab7169745bd82ad395b41710a3e
Habibun Noby <habibun.noby@gmail.com>
2017-02-04 21:16:15 +0600

    seperate container components from presentational components

---
+++ react/LearnReactJS/Part-II/02-AdvancedReact/ContainerComponentsFromPresentationalComponents/02_SeparateContainerComponentsFromPresentationalComponentsApply/components/GuineaPigs.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jony on 2/4/17.
+ */
+
+var React = require('react');
+
+var GuineaPigs = React.createClass({
+    render: function () {
+        var src = this.props.src;
+        return (
+            <div>
+                <h1>Cute Guinea Pigs</h1>
+                <img src={src} />
+            </div>
+        );
+    }
+});
+
+module.exports = GuineaPigs;
