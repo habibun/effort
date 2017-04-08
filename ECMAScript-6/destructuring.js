6b3cf933a2ed9a6fbe002040df9f2490ef3942d3
Habibun Noby <habibun.noby@gmail.com>
2017-04-08 22:26:58 +0600

    destructuring

---
+++ ECMAScript-6/destructuring.js
@@ -0,0 +1,34 @@
+var numbers = [5, 10];
+
+//var number1 = numbers[0];
+//var number2 = numbers[1];
+
+//with destructuring
+
+
+//console.log(number1);
+var [number1, number2] = numbers;
+
+
+//swaping value
+
+[number2, number1] = [number1, number2];
+
+//console.log(number1);
+
+
+//with object
+var dog = {
+	name: 'shorty',
+	age: 22
+}
+
+var {name: dogName, age: dogAge} = dog;
+
+console.log(dogName);
+console.log(dogAge);
+
+
+var {name, age} = dog;
+console.log(name);
+console.log(age);
